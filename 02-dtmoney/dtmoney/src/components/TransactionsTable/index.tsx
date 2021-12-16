1c7d0ef2afa042d8e2a72e4f3707d79b0920315e
hstrada <h.strada@hotmail.com>
2021-12-16 20:50:22 -0300

    add axios with miragejs

--- 02-dtmoney/dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
+import { api } from '../../services/api'
 import { Container } from './styles'
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api
+      .get('http://localhost:3000/api/transactions')
+      .then((data) => console.log(data))
+  }, [])
+
   return (
     <Container>
       <table>
--- 02-dtmoney/dtmoney/src/index.tsx
+++ 02-dtmoney/dtmoney/src/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createServer } from 'miragejs'
+import { App } from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Web',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-18 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Food',
+          amount: 300,
+          createdAt: new Date('2021-02-16 12:00:00'),
+        },
+      ]
+    })
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
