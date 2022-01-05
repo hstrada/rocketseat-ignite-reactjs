ad6a25a2f169f67a8cb16c8237a23406b90235c4
hstrada <h.strada@hotmail.com>
2022-01-05 08:42:29 -0300

    create new transaction modal component

--- 02-dtmoney/dtmoney/src/App.tsx
+++ 02-dtmoney/dtmoney/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -22,12 +23,10 @@ export function App() {
     <div className="App">
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </div>
   );
