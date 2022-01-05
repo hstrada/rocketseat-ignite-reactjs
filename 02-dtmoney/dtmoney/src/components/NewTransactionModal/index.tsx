c795f2f58634b6d27b3e4894b3e580dcb9ea8f2f
hstrada <6079947+hstrada@users.noreply.github.com>
2022-01-05 09:52:21 -0300

    create types button from transaction modal

--- 02-dtmoney/dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-dtmoney/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import outcomeImg from '../../assets/outcome.svg';
+import incomeImg from '../../assets/income.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -31,6 +33,18 @@ export function NewTransactionModal({
 
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
