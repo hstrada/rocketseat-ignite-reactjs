00a03b30b54004a63582b6feef570ae42721b68b
hstrada <6079947+hstrada@users.noreply.github.com>
2022-03-06 18:33:49 -0300

    create transactions context

--- 02-dtmoney/dtmoney/src/components/Summary/index.tsx
+++ 02-dtmoney/dtmoney/src/components/Summary/index.tsx
@@ -1,10 +1,14 @@
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
+import { useContext } from 'react';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 
-import { Container } from './styles'
+import { Container } from './styles';
 
 export function Summary() {
+  const data = useContext(TransactionsContext);
+
   return (
     <Container>
       <div>
@@ -12,28 +16,22 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entrada" />
         </header>
-        <strong>
-            R$ 1000,00
-        </strong>
+        <strong>R$ 1000,00</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saída" />
         </header>
-        <strong>
-            R$ -1000,00
-        </strong>
+        <strong>R$ -1000,00</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>
-            R$ 0,00
-        </strong>
+        <strong>R$ 0,00</strong>
       </div>
     </Container>
-  )
+  );
 }
