71720b14698f3e4a93490004b1e8b1418c3d1237
hstrada <6079947+hstrada@users.noreply.github.com>
2022-03-21 09:14:00 -0300

    create header component

--- 03-ignews/ignews/src/pages/index.tsx
+++ 03-ignews/ignews/src/pages/index.tsx
@@ -1,14 +1,11 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.css';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ignews</title>
       </Head>
-      <h1 className={styles.title}>Hello WOrld</h1>
     </>
   );
 }
