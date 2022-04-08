886884f4356584e72657d7e2e320a9c3b1a91a19
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-08 14:07:59 -0300

    fix folders

--- 03-ignews/ignews/src/components/Button/SignIn/index.tsx
+++ 03-ignews/ignews/src/components/Button/SignIn/index.tsx
@@ -13,14 +13,14 @@ export function SignIn() {
   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      hstrada
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => signIn('github')}
+      onClick={() => signIn()}
     >
       <FaGithub color="#eba417" />
       Sign in with GitHub
