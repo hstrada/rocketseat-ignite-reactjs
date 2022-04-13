b8450556bf080f6e36e133720c147f3a375575f6
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-13 20:45:19 -0300

    add slicemachine from prismic

---
+++ 03-ignews/ignews/.slicemachine/assets/slices/Publications/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Publications';
+
+export default {
+  title: 'slices/Publications'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"publications","items":[],"primary":{"title":[{"type":"heading1","text":"Reinvent intuitive e-commerce","spans":[]}],"description":[{"type":"paragraph","text":"Reprehenderit minim reprehenderit et. Esse aliqua anim voluptate adipisicing eu nisi anim magna mollit ea voluptate adipisicing.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- 03-ignews/ignews/README.md
+++ 03-ignews/ignews/README.md
@@ -5,3 +5,19 @@
 ctrl+c -> ctrl+v
 
 `STRIPE_WEBOOK_SECRET=`
+
+## Prismic.io
+
+### Node Compatible Version
+
+> nvm install 16.3.0
+
+> nvm use 16.3.0
+
+### Install
+
+> npx @slicemachine/init
+
+> yarn run slicemachine
+
+Running on http://localhost:9999
