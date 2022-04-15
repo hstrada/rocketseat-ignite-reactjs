77be65b19caf77d3dd0926b34f4ec732afd97441
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-15 11:45:23 -0300

    create prismic client

--- 03-ignews/ignews/src/pages/posts/index.tsx
+++ 03-ignews/ignews/src/pages/posts/index.tsx
@@ -1,22 +1,38 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { createClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 
 export default function Posts() {
-    return (
-        <>
-            <Head>
-                <title>Posts | ignews</title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Posts | ignews</title>
+      </Head>
 
-            <main className={styles.container}>
-                <div className={styles.posts}>
-                    <a>
-                        <time>18 de março de 2022</time>
-                        <strong>Lorem ipsum generator</strong>
-                        <p>Lorem ipsum generator paragraph description</p>
-                    </a>
-                </div>
-            </main>
-        </>
-    )
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <time>18 de março de 2022</time>
+            <strong>Lorem ipsum generator</strong>
+            <p>Lorem ipsum generator paragraph description</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export async function getStaticProps({ previewData }) {
+  const client = createClient({ previewData })
+
+  const articles = await client.getAllByType('Posts')
+
+  console.log(articles)
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
