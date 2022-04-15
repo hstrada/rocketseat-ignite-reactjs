a36fdf17fca4d45cd4b9c738e163da531d4e3410
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-15 14:21:17 -0300

    create post item

--- 03-ignews/ignews/src/pages/posts/index.tsx
+++ 03-ignews/ignews/src/pages/posts/index.tsx
@@ -1,9 +1,65 @@
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { createClient } from '../../services/prismic'
 import styles from './styles.module.scss'
+import * as prismicH from '@prismicio/helpers'
 
-export default function Posts() {
+const getExcerpt = (slices) => {
+  const text = slices
+    .filter((slice) => slice.slice_type === 'publications')
+    .map((slice) => prismicH.asText(slice.primary.title))
+    .join(' ')
+
+  const excerpt = text.substring(0, 300)
+
+  if (text.length > 300) {
+    return excerpt.substring(0, excerpt.lastIndexOf(' ')) + '…'
+  } else {
+    return excerpt
+  }
+}
+
+const getDescription = (slices) => {
+  const text = slices
+    .filter((slice) => slice.slice_type === 'publications')
+    .map((slice) => prismicH.asText(slice.primary.description))
+    .join(' ')
+
+  const excerpt = text.substring(0, 300)
+
+  if (text.length > 300) {
+    return excerpt.substring(0, excerpt.lastIndexOf(' ')) + '…'
+  } else {
+    return excerpt
+  }
+}
+
+const Post = ({ post }) => {
+  return (
+    <>
+      {post && (
+        <a key={post.key}>
+          <time>{post.updatedAt}</time>
+          <strong>{post.title}</strong>
+          <p>{post.description}</p>
+        </a>
+      )}
+    </>
+  )
+}
+
+interface Post {
+  slug: string
+  key: string
+  updatedAt: string
+  title: string
+  description: string
+}
+
+interface PostsProps {
+  posts: Array<Post>
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,11 +68,9 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>18 de março de 2022</time>
-            <strong>Lorem ipsum generator</strong>
-            <p>Lorem ipsum generator paragraph description</p>
-          </a>
+          {posts.map((post) => (
+            <Post key={post.key} post={post} />
+          ))}
         </div>
       </main>
     </>
@@ -28,11 +82,23 @@ export async function getStaticProps({ previewData }) {
 
   const articles = await client.getAllByType('Posts')
 
-  console.log(articles)
+  const posts = articles.map((post) => {
+    return {
+      slug: post.id,
+      key: post.id,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      title: getExcerpt(post.data.slices),
+      description: getDescription(post.data.slices),
+    }
+  })
 
   return {
     props: {
-      articles,
+      posts,
     },
   }
 }
