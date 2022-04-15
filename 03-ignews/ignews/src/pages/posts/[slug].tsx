90af79d39bd8131ec62804b324133380f25eae31
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-15 20:45:55 -0300

    create post styles page

--- 03-ignews/ignews/src/pages/posts/[slug].tsx
+++ 03-ignews/ignews/src/pages/posts/[slug].tsx
@@ -1,9 +1,9 @@
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
-import * as prismicH from '@prismicio/helpers'
 import { createClient } from '../../services/prismic'
-import { PrismicText } from '@prismicio/react'
 import Head from 'next/head'
+import { PrismicRichText } from '@prismicio/react'
+import styles from './post.module.scss'
 
 export default function Post({ post }) {
   return (
@@ -11,10 +11,14 @@ export default function Post({ post }) {
       <Head>
         <title>{post.title} | ignews</title>
       </Head>
-      <main>
-          <article>
-              
-          </article>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div className={styles.postContent}>
+            <PrismicRichText field={post.description} />
+          </div>
+        </article>
       </main>
     </>
   )
@@ -31,9 +35,24 @@ export const getServerSideProps: GetServerSideProps = async ({
   const client = createClient({ previewData })
   const post = await client.getByUID('Publication', String(slug))
 
+  const formattedPost = {
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      'pt-BR',
+      {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }
+    ),
+    title: post.data.title,
+    description: post.data.description,
+  }
+
+  console.log(formattedPost)
+
   return {
     props: {
-      post,
+      post: formattedPost,
     },
   }
 }
