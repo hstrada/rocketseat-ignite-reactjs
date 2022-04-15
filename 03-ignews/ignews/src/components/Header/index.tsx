8fce316e49b353d000a4780220d759a69f74c283
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-15 14:38:10 -0300

    create active link

--- 03-ignews/ignews/src/components/Header/index.tsx
+++ 03-ignews/ignews/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
-import Button from '../Button';
-import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink'
+
+import Button from '../Button'
+import styles from './styles.module.scss'
 
 export function Header() {
   return (
@@ -7,12 +9,16 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ignews logo" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <Button.SignIn />
       </div>
     </header>
-  );
+  )
 }
