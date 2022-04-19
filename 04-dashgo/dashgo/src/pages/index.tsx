946c95659e53aec0878e310dbd68b4f1ac028756
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-19 08:04:24 -0300

    create input component

--- 04-dashgo/dashgo/src/pages/index.tsx
+++ 04-dashgo/dashgo/src/pages/index.tsx
@@ -1,3 +1,27 @@
+import { Button, Flex, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
 export default function Home() {
-  return <h1>Hwllo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail" />
+
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+        <Button type="submit" size="lg" mt={6} colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
