3865144e6478f0326bf0d808ff4fa08b4e500f59
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-19 12:59:32 -0300

    create dashboard

--- 04-dashgo/dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/dashgo/src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react';
 import { Header } from '../components/Header';
 import { SideBar } from '../components/Sidebar';
 
@@ -9,6 +9,24 @@ export default function Dashboard() {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <SideBar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignContent="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
