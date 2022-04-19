bf57f900338cdf749a9773fd015e7c7e7364876c
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-19 19:13:41 -0300

    modify dashboard with graph

--- 04-dashgo/dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/dashgo/src/pages/dashboard.tsx
@@ -1,6 +1,13 @@
+import dynamic from 'next/dynamic';
+const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
+
 import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react';
 import { Header } from '../components/Header';
 import { SideBar } from '../components/Sidebar';
+
+const options = {};
+
+const series = [{ name: 'series 1', data: [31, 120, 10, 28, 51, 18, 64] }];
 
 export default function Dashboard() {
   return (
@@ -20,6 +27,14 @@ export default function Dashboard() {
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
+            {typeof window !== 'undefined' && (
+              <ApexCharts
+                type="area"
+                height={160}
+                options={options}
+                series={series}
+              />
+            )}
           </Box>
           <Box p="8" bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
