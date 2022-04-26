fd68ca916592572a26a590a61fecc7f76a59b01d
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-26 08:14:30 -0300

    usebreakpoint value on header component

--- 04-dashgo/dashgo/src/components/Header/Profile.tsx
+++ 04-dashgo/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Helena Strada</Text>
-        <Text color="gray.300" fontSize="small">
-          hstrada407@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Helena Strada</Text>
+          <Text color="gray.300" fontSize="small">
+            hstrada407@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
