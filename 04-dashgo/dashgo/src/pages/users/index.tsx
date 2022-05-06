b934b9dfbd6905264863b6509645c15fcfd96e9a
hstrada <6079947+hstrada@users.noreply.github.com>
2022-05-06 17:22:27 -0300

    adding react-query with faker

--- 04-dashgo/dashgo/src/pages/users/index.tsx
+++ 04-dashgo/dashgo/src/pages/users/index.tsx
@@ -12,9 +12,11 @@ import {
   Th,
   Thead,
   Tr,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useQuery } from 'react-query';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
@@ -25,6 +27,81 @@ export default function UserList() {
     base: false,
     lg: true
   });
+
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users');
+      const data = await response.json();
+
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+          })
+        };
+      });
+
+      return users;
+    },
+    {
+      staleTime: 1000 * 5 // 5 segundos
+    }
+  );
+
+  const UsersData = () => (
+    <>
+      <Table colorScheme="whiteAlpha">
+        <Thead>
+          <Tr>
+            <Th px={['4', '4', '6']} color="gray.300" width="8">
+              <Checkbox colorScheme="pink" />
+            </Th>
+            <Th>Usuário</Th>
+            {isWideVersion && <Th>Data de cadastro</Th>}
+            <Th width="8"></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((user) => {
+            return (
+              <Tr key={user.id}>
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">{user.name}</Text>
+                    <Text fontSize="small" color="gray.300">
+                      {user.email}
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>{user.createdAt}</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+      <Pagination />
+    </>
+  );
 
   return (
     <Box>
@@ -52,46 +129,17 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">helena strada</Text>
-                    <Text fontSize="small" color="gray.300">
-                      hroad@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>18 de março de 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="small"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados do usuário</Text>
+            </Flex>
+          ) : (
+            <UsersData />
+          )}
         </Box>
       </Flex>
     </Box>
---
+++ 04-dashgo/dashgo/src/services/mirage/index.ts
@@ -0,0 +1,47 @@
+import { createServer, Factory, Model } from 'miragejs';
+import { faker } from '@faker-js/faker';
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(index: number) {
+          return `TestUser ${index + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10, new Date());
+        }
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 10);
+    },
+
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750; // 750ms para toda chamada ser finalizada
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough();
+    }
+  });
+
+  return server;
+}
