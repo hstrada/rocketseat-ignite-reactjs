556791a04feaf764c16172d67a3f2f5403b34b14
hstrada <6079947+hstrada@users.noreply.github.com>
2022-05-06 18:08:39 -0300

    create mirage pagination

--- 04-dashgo/dashgo/src/pages/users/index.tsx
+++ 04-dashgo/dashgo/src/pages/users/index.tsx
@@ -21,6 +21,7 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { SideBar } from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
@@ -28,31 +29,7 @@ export default function UserList() {
     lg: true
   });
 
-  const { data, isLoading, error } = useQuery(
-    'users',
-    async () => {
-      const response = await fetch('http://localhost:3000/api/users');
-      const data = await response.json();
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-          })
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5 // 5 segundos
-    }
-  );
+  const { data, isLoading, error, isFetching } = useUsers();
 
   const UsersData = () => (
     <>
@@ -114,6 +91,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="small" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
