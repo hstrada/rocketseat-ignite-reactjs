bd2b940b3ec579cbdd207eb08eb6d42a42900562
hstrada <6079947+hstrada@users.noreply.github.com>
2022-05-08 15:14:29 -0300

    adding useMemo

--- 05-performance/performance/src/components/SearchResults.tsx
+++ 05-performance/performance/src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { ProductItem } from './ProductItem'
 
 type SearchResultProps = {
@@ -9,11 +10,21 @@ type SearchResultProps = {
 }
 
 export function SearchResults({ results }: SearchResultProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+    }, 0)
+    // quando quer o calculo seja refeito
+  }, [results]) // somente quando os resultados da busca mudarem
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map((product) => {
         return <ProductItem product={product} />
       })}
     </div>
   )
 }
+
+// useMemo -> calculos pesados, igualdade referencial (repassa a informacao a um componente filho)
